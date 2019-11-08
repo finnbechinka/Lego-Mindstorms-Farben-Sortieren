e6a651edb2f4eaed90cb5752a214008fd6637720
Finn <26581865+shakerrrr@users.noreply.github.com>
2019-11-08 11:06:12 +0100

    pilot calibration

---
+++ src/program/Calibration.java
@@ -0,0 +1,39 @@
+package program;
+
+import lejos.hardware.lcd.LCD;
+import lejos.robotics.navigation.MovePilot;
+import lejos.utility.Delay;
+
+public class Calibration {
+	MovePilot pilot;
+	
+	public Calibration(MovePilot pilot){
+		this.pilot = pilot;
+		this.pilot.setLinearSpeed(20);
+		this.pilot.setAngularSpeed(100);
+		this.pilot.setAngularAcceleration(10);
+		this.pilot.setLinearAcceleration(4);
+		test();
+	}
+	
+	public void rotate(int deg){
+		LCD.clear();
+		LCD.drawString("rotate " + deg + "°", 0, 4);
+		Delay.msDelay(1000);
+		pilot.rotate(deg);
+	}
+	
+	public void drive(int dist){
+		LCD.clear();
+		LCD.drawString("drive " + dist + "cm", 0, 4);
+		Delay.msDelay(1000);
+		pilot.travel(dist);
+	}
+	
+	public void test(){
+		rotate(360);
+		drive(100);
+		rotate(180);
+		drive(100);
+	}
+}
--- src/program/Main.java
+++ src/program/Main.java
@@ -14,56 +14,36 @@ import lejos.robotics.navigation.*;
 import lejos.utility.*;
 
 public class Main {
+	
+	static RegulatedMotor motorA = new EV3LargeRegulatedMotor(MotorPort.B);
+	static RegulatedMotor motorB = new EV3LargeRegulatedMotor(MotorPort.C);
+	static EV3ColorSensor colorSensor = new EV3ColorSensor(SensorPort.S4);
+
+	//Wheel diameter 5.6cm (Calibrated to L5.6 and R5.55)
+	//Track width 12cm (Calibrated to 11cm);
+	static Wheel leftWheel = new WheeledChassis.Modeler(motorA, 5.6).offset(-5.5);
+	static Wheel rightWheel = new WheeledChassis.Modeler(motorB, 5.55).offset(5.5);
+	static Chassis chassis = new WheeledChassis(new Wheel[]{leftWheel, rightWheel}, WheeledChassis.TYPE_DIFFERENTIAL); 
+	static MovePilot pilot = new MovePilot(chassis);
 
 	public static void main(String[] args) {
-		LCD.drawString("gyro and pilot", 0, 3);
-		LCD.drawString("navigation test 2", 0, 4);
-		Delay.msDelay(3000);
-		
-		
-		RegulatedMotor motorA = new EV3LargeRegulatedMotor(MotorPort.C);
-		RegulatedMotor motorB = new EV3LargeRegulatedMotor(MotorPort.B);
-		// EV3ColorSensor colorSensor = new EV3ColorSensor(SensorPort.S4);
-		@SuppressWarnings("resource")
-		EV3GyroSensor gyroSensor = new EV3GyroSensor(SensorPort.S2);
-
-		//Wheel diameter 5.5
-		//Track width 12
-		Wheel leftWheel = new WheeledChassis.Modeler(motorA, 5.5).offset(-6);
-		Wheel rightWheel = new WheeledChassis.Modeler(motorB, 5.5).offset(6);
-		Chassis chassis = new WheeledChassis(new Wheel[]{leftWheel, rightWheel}, WheeledChassis.TYPE_DIFFERENTIAL); 
-		MovePilot pilot = new MovePilot(chassis);
-		
-		pilot.setLinearSpeed(10);
-		pilot.setAngularSpeed(100);
-				
-		
-		pilot.travel(50);
-		
-		pilot.rotate(180);
-		
-		
-//		final SampleProvider sp = gyroSensor.getAngleMode();
-//		float[] angleSample = new float[sp.sampleSize()];
-//		sp.fetchSample(angleSample, 0);
-//		
-//		while(angleSample[0] <= 180){
-//			pilot.rotateRight();
-//			sp.fetchSample(angleSample, 0);
-//			
-//		}
-		
-		pilot.travel(50);
+		new Calibration(pilot);
 		
 		Sound.beep();
 		LCD.clear();
 		LCD.drawString("END OF PROGRAM", 0, 4);
 		Delay.msDelay(2000);
-		Sound.twoBeeps();
 		
 		LCD.clear();
 		LCD.drawString("PRESS ENTER TO EXIT", 0, 4);
 		Button.ENTER.waitForPress();
+	}
+	
+	public Main(){
+		LCD.drawString("pilot", 0, 3);
+		LCD.drawString("navigation test", 0, 4);
+		Delay.msDelay(3000);
+		
 	}
 
 }
