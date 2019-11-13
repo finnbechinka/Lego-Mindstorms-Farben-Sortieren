4478d5c1420abb693791dd6cc49ec44fe97b9c89
Finn <contact@bechinka.eu>
2019-11-13 20:04:40 +0100

    eclipse upload

---
+++ Lego-Mindstorms-Farben-Sortieren/src/program/Main.java
@@ -0,0 +1,51 @@
+package program;
+
+import lejos.hardware.Sound;
+import lejos.hardware.lcd.LCD;
+import lejos.hardware.motor.*;
+import lejos.hardware.port.*;
+import lejos.hardware.sensor.*;
+import lejos.hardware.Button;
+import lejos.robotics.*;
+import lejos.robotics.chassis.Chassis;
+import lejos.robotics.chassis.Wheel;
+import lejos.robotics.chassis.WheeledChassis;
+import lejos.robotics.navigation.*;
+import lejos.utility.*;
+
+public class Main {
+	
+	static RegulatedMotor motorA = new EV3LargeRegulatedMotor(MotorPort.B);
+	static RegulatedMotor motorB = new EV3LargeRegulatedMotor(MotorPort.C);
+	static EV3ColorSensor colorSensor = new EV3ColorSensor(SensorPort.S4);
+
+	//Wheel diameter 5.6cm (Calibrated to L5.6 and R5.55)
+	//Track width 12cm (Calibrated to 11cm);
+	static Wheel leftWheel = new WheeledChassis.Modeler(motorA, 5.6).offset(-5.5);
+	static Wheel rightWheel = new WheeledChassis.Modeler(motorB, 5.55).offset(5.5);
+	static Chassis chassis = new WheeledChassis(new Wheel[]{leftWheel, rightWheel}, WheeledChassis.TYPE_DIFFERENTIAL); 
+	static MovePilot pilot = new MovePilot(chassis);
+	
+	static int[] pos = new int[]{0,0};
+
+	public static void main(String[] args) {
+		new Calibration(pilot);
+		
+		Sound.beep();
+		LCD.clear();
+		LCD.drawString("END OF PROGRAM", 0, 4);
+		Delay.msDelay(2000);
+		
+		LCD.clear();
+		LCD.drawString("PRESS ENTER TO EXIT", 0, 4);
+		Button.ENTER.waitForPress();
+	}
+	
+	public Main(){
+		LCD.drawString("pilot", 0, 3);
+		LCD.drawString("navigation test", 0, 4);
+		Delay.msDelay(3000);
+		
+	}
+
+}
